b55c99f6b834d2cc922b588960a11922bf0fb4c2
James Ginns <starvagrant@yahoo.com>
2019-12-16 15:43:35 -0600

    Create satellite class to hold json data
    Create a satellite class that will hold json data to be fetched
    later.

---
+++ src/app/satellite.ts
@@ -0,0 +1,12 @@
+export class Satellite {
+
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
+
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
+
+    }
+}
