339d129dc83348a6713f0255cfe38fe586e6b4be
James Ginns <starvagrant@yahoo.com>
2019-12-18 19:13:11 -0600

    Load satellite table from json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,26 @@ import { Satellite } from './satellite';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'orbit-report';
 
-  sourceList: Satellite[];
+  sourceList: Satellite[] = [];
 
   constructor() {
-    this.sourceList = [
-      new Satellite("Sirius XM", "Communication", "2009-03-21", "LOW", true),
-      new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-      new Satellite("Weber Grill", "Space Debris", "1996-03-25","HIGH", false),
-      new Satellite("GPS 938", "Positioning", "2001-11-01", "High", true),
-      new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true)
-    ]
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+    window.fetch(satellitesUrl).then(function(response) {
+        response.json().then(function(data) {
+
+          let fetchedSatellites = data.satellites;
+          for(let i=0; i<data.satellites.length; i++) {
+            let satellite = new Satellite(fetchedSatellites[i].name,
+              fetchedSatellites[i].type, fetchedSatellites[i].launchDate,
+              fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+            this.sourceList.push(satellite);
+          }
+        }.bind(this));
+    }.bind(this));
   }
 }
