d6d7e89d9352f94fd4d9458e3fa9a11fba6e5ca1
James Ginns <starvagrant@yahoo.com>
2019-12-20 23:15:28 -0600

    Add the same satellite input to orbit counts
    Bind the same satellite data to orbit counts as is done in the
    list. How best to access it remains keys.

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Satellite } from '../satellite';
 @Component({
   selector: 'app-orbit-counts',
   templateUrl: './orbit-counts.component.html',
   styleUrls: ['./orbit-counts.component.css']
 })
 export class OrbitCountsComponent implements OnInit {
+  @Input() satellites: Satellite[]
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  getCount(type: String) {
+    let count: number = 0;
+    for(let i=0; i<this.satellites.length; i++) {
+      if (this.satellites[i].type.toLowerCase() === type.toLowerCase()) count++;
+    }
+    return count;
+  }
 }
